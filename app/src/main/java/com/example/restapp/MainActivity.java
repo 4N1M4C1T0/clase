5f55a08fc3b93f09a1a35d6ef41c76a21e29dd77
4N1M4C1T0 <richardmeneses2110@gmail.com>
2020-12-22 23:28:32 -0500

    avance

--- app/src/main/java/com/example/restapp/MainActivity.java
+++ app/src/main/java/com/example/restapp/MainActivity.java
@@ -3,12 +3,46 @@ package com.example.restapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvResultados;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvResultados = findViewById(R.id.tvResultados);
+    }
+
+    public void TraerDatos(View view) {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url = "http:/localhost/servidor/servidor.php?hash=4512348";
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        tvResultados.setText(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("RestApp", error.getMessage());
+                    }
+                }
+        );
     }
 }
